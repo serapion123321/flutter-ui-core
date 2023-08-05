9a400cb5725b6a42b973171034fa548811071846
DESKTOP-ECIDV4D\user <seratrain1600@gmail.com>
2023-08-05 21:25:04 +0700

    [=] add function to change header font size

--- lib/flutter_ui_core.dart
+++ lib/flutter_ui_core.dart
@@ -1,22 +1,15 @@
 library flutter_ui_core;
-
 import 'package:flutter_ui_core/presentation/utility/typography.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
 
 class InitialValue{
   static setBodyFontSize({
-    double? bodyTextXxl = 28,
-    double? bodyTextXl = 24,
-    double? bodyTextL = 20,
-    double? bodyTextM = 16,
-    double? bodyTextS = 12,
-    double? bodyTextXs = 10,
-    double? bodyTextXxs = 8,
+    double? bodyTextXxl,
+    double? bodyTextXl,
+    double? bodyTextL,
+    double? bodyTextM,
+    double? bodyTextS,
+    double? bodyTextXs,
+    double? bodyTextXxs,
   }){
     StandardFontSize.bodyTextXxl = bodyTextXxl ?? 28;
     StandardFontSize.bodyTextXl = bodyTextXl ?? 24;
@@ -26,6 +19,20 @@ class InitialValue{
     StandardFontSize.bodyTextXs = bodyTextXs ?? 10;
     StandardFontSize.bodyTextXxs = bodyTextXxs ?? 8;
   }
-  
 
+  static setHeaderFontSize({
+    double? h1,
+    double? h2,
+    double? h3,
+    double? h4,
+    double? h5,
+    double? h6,
+  }){
+    StandardFontSize.h1 = h1 ?? 32;
+    StandardFontSize.h2 = h2 ?? 28;
+    StandardFontSize.h3 = h3 ?? 24;
+    StandardFontSize.h4 = h4 ?? 20;
+    StandardFontSize.h5 = h5 ?? 16;
+    StandardFontSize.h6 = h6 ?? 12;
+  }
 }
