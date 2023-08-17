f9b7f6d8b2bc76a91e87d789daab82730ac179b0
DESKTOP-ECIDV4D\user <seratrain1600@gmail.com>
2023-08-17 17:48:18 +0700

    - feat light fontWeight

--- lib/presentation/utility/typography.dart
+++ lib/presentation/utility/typography.dart
@@ -19,6 +19,7 @@ class StandardFontSize {
 }
 
 class StandardFontWeight {
+  static const FontWeight light = FontWeight.w300;
   static const FontWeight regular = FontWeight.w400;
   static const FontWeight medium = FontWeight.w500;
   static const FontWeight semiBold = FontWeight.w600;
