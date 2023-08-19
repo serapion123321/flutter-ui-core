6037d30e8c5973cac06c16267297496c4ec99740
DESKTOP-ECIDV4D\user <seratrain1600@gmail.com>
2023-08-19 19:31:01 +0700

    - feat button

--- lib/presentation/components/button/button.dart
+++ lib/presentation/components/button/button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_core/presentation/utility/utility.dart';
 
 enum IconPosition{
   left, right
@@ -8,19 +9,26 @@ class StandardButton {
   Widget primaryButton({
     required title,
     double? borderRadius,
+    double? elevation,
     bool? isLoading,
     bool? isDisabled,
     Color? backgroundColor,
     Color? titleColor,
+    Color? splashColor,
     Function()? onTap,
     IconPosition? iconPosition,
     Widget? icon
   }) {
     return Material(
+      color: backgroundColor ?? ColorTheme.primary700,
+      elevation: elevation ?? 1,
       child: InkWell(
+        splashColor: splashColor ?? ColorTheme.white,
+        borderRadius: BorderRadius.circular(borderRadius ?? 12),
         onTap: onTap,
         child: Container(
-          child: Text("Oke"),
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: standardBodyText(text: title),
         ),
       ),
     );
