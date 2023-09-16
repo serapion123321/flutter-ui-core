4f98e7c078e2755779e00dd3387bb6cbb4ef1711
DESKTOP-ECIDV4D\user <seratrain1600@gmail.com>
2023-09-16 11:18:41 +0700

    - feat chip

---
+++ lib/presentation/components/chip/chip.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_core/presentation/utility/utility.dart';
+
+enum ChipSize{
+  large,
+  medium,
+  small,
+}
+
+double funcFontSizeConverter(ChipSize buttonSize){
+  switch (buttonSize) {
+    case ChipSize.large:
+      return StandardFontSize.h4;
+    case ChipSize.medium:
+      return StandardFontSize.h5;
+    case ChipSize.small:
+      return StandardFontSize.h6;
+  }
+}
+
+
+class StandardChip {
+  Widget chip({
+    required String title,
+    EdgeInsets? margin,
+    ChipSize? chipSize,
+    double? borderRadius,
+    double? elevation,
+    bool isDisabled = false,
+    bool isUsingCloseIcon = false,
+    Color? backgroundColor,
+    Color? disabledBackgroundColor,
+    Color? titleColor,
+    Color? disabledTitleColor,
+    Color? splashColor,
+    Color? disabledSplashColor,
+    Color? borderColor,
+    Color? disabledBorderColor,
+    Function()? onTap,
+    Function()? onTapClose,
+    Widget? iconWidget,
+    Widget iconCloseWidget = const SizedBox()
+  }){
+    return Padding(
+      padding : margin ?? const EdgeInsets.symmetric(vertical: 4),
+      child: Material(
+        color: isDisabled
+            ? disabledBackgroundColor ?? ColorTheme.grey700
+            : backgroundColor ?? ColorTheme.primary700,
+        elevation: elevation ?? 1,
+        borderRadius: BorderRadius.circular(borderRadius ?? 12),
+        child: InkWell(
+          splashColor: isDisabled ? Colors.transparent : splashColor ?? ColorTheme.white,
+          borderRadius: BorderRadius.circular(borderRadius ?? 12),
+          onTap: isDisabled ? (){} : onTap,
+          child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(borderRadius ?? 12),
+                  border: Border.all(
+                      color: isDisabled
+                          ? disabledBorderColor ?? ColorTheme.black
+                          : borderColor ?? ColorTheme.primary500
+                  )
+              ),
+              child: childChipWidget(
+                  iconCloseWidget: isUsingCloseIcon
+                      ? iconCloseWidget
+                      : IconButton(
+                          onPressed: onTapClose,
+                          icon: Icon(
+                            Icons.close,
+                            color: isDisabled
+                                ? ColorTheme.grey500
+                                : ColorTheme.danger500
+                          ),
+                  ),
+                  body: standardHeaderText(
+                      fontSize: funcFontSizeConverter(chipSize ?? ChipSize.medium),
+                      text: title,
+                      color: isDisabled
+                          ? disabledTitleColor ?? ColorTheme.black
+                          : titleColor ?? ColorTheme.black
+                  ),
+              )
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget childChipWidget({
+    required Widget body,
+    required Widget iconCloseWidget,
+    Widget? icon
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        icon ?? const SizedBox(),
+        body,
+        iconCloseWidget,
+      ],
+    );
+  }
+}
